b581c106491d604ae120b39672fe7877696bbf83
fajar-dwi <56991659+fajar-dwi@users.noreply.github.com>
2024-12-06 11:00:41 +0700

    lihat data harian anak

--- registrasi/application/views/inc/aktivitashariananak/list.php
+++ registrasi/application/views/inc/aktivitashariananak/list.php
@@ -49,7 +49,7 @@
                                                         <label>Tanggal</label>
                                                     </td>
                                                     <td>
-                                                        <select class="form-control" id="id_rincianjadwal_mingguan" name="id_rincianjadwal_mingguan" required>
+                                                        <select class="form-control select2" id="id_rincianjadwal_mingguan" name="id_rincianjadwal_mingguan" required>
                                                             <?php foreach ($tanggal as $key => $value) { ?>
                                                                 <option value="<?= $value->id_rincianjadwal_mingguan ?>" <?= $id_rincianjadwal_mingguan == $value->id_rincianjadwal_mingguan ? 'selected' : '' ?>><?= 'Tema: '.$value->nama_tema.', '.format_date_indonesia($value->tanggal).' '.date('d-m-Y', strtotime($value->tanggal)).' ('.$value->nama_subtema.')' ?></option>
                                                             <?php } ?>
