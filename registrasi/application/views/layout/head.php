eff63f6050c3db2607ff59f564b27b5bf0de23b2
amanah-teknologi-digital <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>
2025-05-12 19:10:49 +0700

    update almahyra desain

--- registrasi/application/views/layout/head.php
+++ registrasi/application/views/layout/head.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Namira Plus Moslem Daycare</title>
+    <title>Almahyra</title>
     <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url().'dist-assets/'?>fav/icon_namira.png">
     <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,400i,600,700,800,900" rel="stylesheet" />
     <link href="<?= base_url().'dist-assets/'?>css/themes/lite-purple.min.css" rel="stylesheet" />
