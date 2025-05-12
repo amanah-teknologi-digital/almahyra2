92f1832ec88704d8a0410c47e3dab13c432044d0
amanah-teknologi-digital <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>
2025-05-12 19:23:01 +0700

    update almahyra footer

--- registrasi/application/views/layout/footer.php
+++ registrasi/application/views/layout/footer.php
@@ -4,8 +4,7 @@
         <div class="d-flex align-items-center">
             <img class="logo" src="<?= base_url().'dist-assets/'?>/images/logo_almahyra_panjang.png" alt="" style="width: 80px; height: 40px">
             <div class="ml-2">
-                <p class="m-0">&copy; <?= Date('Y') ?> Almahyra</p>
-                <p class="m-0">All rights reserved</p>
+                <p class="m-0">&copy; <?= Date('Y') ?> Almahyra, All rights reserved</p>
             </div>
         </div>
     </div>
