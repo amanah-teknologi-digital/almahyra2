500b91046fa5faa872edb617f7c66c09a8b1c9e4
amanah-teknologi-digital <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>
2025-05-12 19:21:56 +0700

    update almahyra footer

--- registrasi/application/views/layout/footer.php
+++ registrasi/application/views/layout/footer.php
@@ -2,9 +2,9 @@
 <div class="app-footer">
     <div class="footer-bottom pt-3 d-flex flex-column flex-sm-row align-items-center">
         <div class="d-flex align-items-center">
-            <img class="logo" src="<?= base_url().'dist-assets/'?>/images/logo_almahyra_panjang.png" alt="" style="width: 170px; height: 65px">
+            <img class="logo" src="<?= base_url().'dist-assets/'?>/images/logo_almahyra_panjang.png" alt="" style="width: 80px; height: 40px">
             <div class="ml-2">
-                <p class="m-0">&copy; 2022 Namira Plus Moslem Daycare</p>
+                <p class="m-0">&copy; <?= Date('Y') ?> Almahyra</p>
                 <p class="m-0">All rights reserved</p>
             </div>
         </div>
